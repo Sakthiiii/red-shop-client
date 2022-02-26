5d40923ccd226de420a8460de10dd897b0bee3a1
Sakthi <87663226+Sakthiiii@users.noreply.github.com>
2022-02-26 14:48:55 +0530

    Updates

---
+++ precache-manifest.b085139ec6cdb858917792c89984c46f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "227306658a2086a85113c9d6109e0813",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d5346cd2b7182cfdb0d",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "7e800502fe67a1f8fa6f",
+    "url": "/static/js/2.61ecbd3d.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/static/js/2.61ecbd3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d5346cd2b7182cfdb0d",
+    "url": "/static/js/main.b1a31f2b.chunk.js"
+  },
+  {
+    "revision": "884421c92f43d3cab84a",
+    "url": "/static/js/runtime-main.b7c8601d.js"
+  },
+  {
+    "revision": "55bc815e591dc8600439bd9bbdf4d451",
+    "url": "/static/media/2.55bc815e.jpg"
+  }
+]);
